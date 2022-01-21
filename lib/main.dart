6c784111372e8d73a555e62278bd198181ab9388
Arhe1 <scherbachek@gmail.com>
2022-01-21 16:53:05 +0700

    добавил фоновое изображение и логотип

--- lib/main.dart
+++ lib/main.dart
@@ -23,59 +23,69 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body:Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/space3.jpeg'),
+              fit: BoxFit.cover,
+            )
+          ),
           width: double.infinity,
-          child: Column(children: [
-            SizedBox(height: 80,),
-            SizedBox(width: 110, height: 84, child: Placeholder(),),
-            SizedBox(height: 20,),
-            Text('Введите логин в виде 10 цифр номера телефона',
-            style: TextStyle(fontSize: 16, color: Color.fromRGBO(0, 0, 0, 0.6)),),
-            SizedBox(height: 20,),
-            SizedBox(width: 244,
-              child: TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Color(0xFFeceff1),
-                  enabledBorder: borderStyle,
-                  focusedBorder: borderStyle,
-                  labelText: 'Телефон',
+          child: SingleChildScrollView(
+            child: Column(children: [
+              SizedBox(height: 150,),
+              const SizedBox(width: 130, height: 94, child: const Image(image: AssetImage('assets/space2.png')),),
+              SizedBox(height: 20,),
+              Text('Введите логин в виде 10 цифр номера телефона',
+              style: TextStyle(fontSize: 16, color: Color.fromRGBO(0, 0, 0, 0.6)),),
+              SizedBox(height: 20,),
+              SizedBox(width: 244,
+                child: TextField(
+                  keyboardType: TextInputType.phone,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Color(0xFFeceff1),
+                    enabledBorder: borderStyle,
+                    focusedBorder: borderStyle,
+                    labelText: 'Телефон',
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: 20,),
-            SizedBox(width: 244,
-              child: TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Color(0xFFeceff1),
-                  enabledBorder: borderStyle,
-                  focusedBorder: borderStyle,
-                  labelText: 'Пароль',
+              SizedBox(height: 20,),
+              SizedBox(width: 244,
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Color(0xFFeceff1),
+                    enabledBorder: borderStyle,
+                    focusedBorder: borderStyle,
+                    labelText: 'Пароль',
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: 28,),
-            SizedBox(width: 154, height: 42,child:
-             ElevatedButton(onPressed: () {},
-                child: Text('Войти'),
-                 style: ElevatedButton.styleFrom(primary: Color(0xFF0079D0),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(36.0),
+              SizedBox(height: 28,),
+              SizedBox(width: 154, height: 42,child:
+               ElevatedButton(onPressed: () {},
+                  child: Text('Войти'),
+                   style: ElevatedButton.styleFrom(primary: Color(0xFF0079D0),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(36.0),
+                ),
               ),
-            ),
-             ),
-            ),
+               ),
+              ),
 
 
 
-            SizedBox(height: 62,),
-            InkWell(child: const Text('Регистрация',
-              style: linkTextStyle), onTap:() {}),
-            SizedBox(height: 20,),
-            InkWell(child: Text('Забыли пароль',
-              style: linkTextStyle,), onTap:() {}),
-          ],),
+              SizedBox(height: 62,),
+              InkWell(child: const Text('Регистрация',
+                style: linkTextStyle), onTap:() {}),
+              SizedBox(height: 20,),
+              InkWell(child: Text('Забыли пароль',
+                style: linkTextStyle,), onTap:() {}),
+            ],),
+          ),
         )
       ),
     );
